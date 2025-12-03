096adcee348e8eb24dddf16be89cf088b93529c7
WanAriff22 <s75565@ocean.umt.edu.my>
2025-12-03 21:04:44 +0800

    Delete lab6 directory

--- lab6/README.md
+++
@@ -1,1 +0,0 @@
-This lab teaches me how to use JavaScript. By using JavaScript, my website has become more interactive with user input.
--- lab6/form-validation.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-  let fname = document.forms["myForm"]["fname"].value;
-  if (fname === "") {
-    alert("First Name must be filled out.");
-    return false;
-  }
-}
